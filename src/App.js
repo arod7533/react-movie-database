2a57a99fb110c0037c5c4c1c7e0c33e92687d974
arod7533 <arod7533@gmail.com>
2021-10-12 18:16:32 -0400

    add routing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import React from 'react';
 import Header from './components/Header/Header.component';
 import Home from './components/Home';
+import Movie from './components/Movie';
+import NotFound from './components/NotFound';
+
+//Routing
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 //Styles
 import { GlobalStyle } from './GlobalStyle';
 
-function App() {
-  return (
-    <div className="App">
+const App = () => (
+    <Router>
       <Header />
-      <Home />
+      <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/:movieId' element={<Movie/>} />
+          <Route path='/*' element={<NotFound/>} />
+      </Routes>
       <GlobalStyle />
-    </div>
+    </Router>
   );
-}
+
 
 export default App;
