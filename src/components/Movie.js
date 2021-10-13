3a6bda7d68a14c172e5f821900739b6340c25ed6
arod7533 <arod7533@gmail.com>
2021-10-13 08:40:19 -0400

    breadcrumb creation and movie hook

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,5 +1,29 @@
 import react from "react";
+import { useParams } from "react-router";
 
-const Movie = () => <div>Movie</div>
+import {IMAGE_BASE_URL, POSTER_SIZE} from '../config';
+
+import { useMovieFetch } from "../hooks/userMovieFetch";
+
+import Grid from './Grid/Grid.component'
+import Spinner from './Spinner/Spinner.component'
+import BreadCrumb from "./BreadCrumb/BreadCrumb.component";
+
+import NoImage from '../images/no_image.jpg';
+
+const Movie = () => {
+    const {movieId} = useParams();
+
+    const { state: movie, loading, error } = useMovieFetch(movieId);
+
+    if (loading) return <Spinner />;
+    if (error) return <div>Something went wrong...</div>;
+
+    return (
+        <>
+            <BreadCrumb movieTitle={movie.original_title}></BreadCrumb>
+        </>
+    );
+};
 
 export default Movie;
